87b0c13196f61a8347c35015a56b059f8b709d01
grechishko <42656849+grechishko@users.noreply.github.com>
2018-12-23 09:23:04 +0300

    add lot

--- main.js
+++ main.js
@@ -1,91 +1,42 @@
 const express = require('express');
-const app = express();
-
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// App Init
+const app = express();
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// DB
 mongoose.connect('mongodb://localhost/auc', { useNewUrlParser: true });
 const db = mongoose.connection;
-
-
-// Check connection
 db.once('open', function(){
     console.log('Connected to MongoDB');
 });
-// Check for DB errors
 db.on('error', function(err){
     console.log(err);
 });
 
 
-// Bring in Models
-let Lots = require('./models/lots');
+// Models
+let Lot = require('./models/lot');
 
 
 
 
-// var store = {
-//         home: {
-//             page: "Main page",
-//             content: "Home, sweet home"
-//         },
-//         about: {
-//             page: "About page",
-//             content: "This is about page"
-//         },
-//         lot: {
-//             page: "Страница лота",
-//             content: "Лот"
-//         },
-//         search: {
-//             page: "Поиск",
-//             content: "поиск"
-//         }
-//     },
-//     storeKeys = Object.keys(store);
 
-
-app.set('view engine', 'jade');
-
+// Middleware
 app.use(function(req, res, next){
     console.log('%s %s', req.method, req.url);
     next();
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
 
-
-// app.get('/about', function(req, res){
-//     res.render('about', {
-//         page: 'About Page',
-//         links: storeKeys
-//     });
-// });
-
-
-
-// app.route('/new')
-//     .get(function(req, res){
-//         res.render('new', {
-//             page: 'Add New',
-//             links: storeKeys
-//         });
-//     })
-//     .post(function(req, res){
-//         let data = req.body;
-//         if (data.pageurl && data.pagename && data.pagecontent) {
-//             store[data.pageurl] = {
-//                 page: data.pagename,
-//                 content: data.pagecontent
-//             };
-//             storeKeys = Object.keys(store);
-//         }
-//         res.redirect('/');
-//     });
 
 app.get('/lots', function(req, res){
-    Lots.find({}, function(err, lots){
+    Lot.find({}, function(err, lots){
         if (err) {
             console.log(err);
             return;
@@ -106,7 +57,7 @@ app.get('/lots/:id', function(req, res){
         res.redirect('/lots');
         return;
     }
-    Lots.findOne({_id: id }, function(err, lot){
+    Lot.findOne({_id: id }, function(err, lot){
         if (err) {
             console.log('Error:\n')
             console.log(err);
@@ -126,45 +77,37 @@ app.route('/signup')
         });
     })
     .post(function(req, res){
-        let data = req.body;
-        // if (data.pageurl && data.pagename && data.pagecontent) {
-        //     store[data.pageurl] = {
-        //         page: data.pagename,
-        //         content: data.pagecontent
-        //     };
-        //     storeKeys = Object.keys(store);
-        // }
-        // res.redirect('/');
+        // let data = req.body;        
     });
 
-// app.get('/:page?', function(req, res){
-//     let page = req.params.page;
-//     if (!page) page = 'home';
-//     let data = store[page];
-//     if (!data) {
-//         res.redirect('/');
-//         return;
-//     }
-//     data.links = storeKeys;
-//     data.query = req.query;
-//     res.render('main', data);
-// });
+
+app.route('/add')
+    .get(function(req, res){
+        res.render('add', {
+        });
+    })
+    .post(function(req, res){
+        let data = req.body;        
+        console.log(data);
+
+        let newLot = new Lot();
+        newLot.name = data.name;
+        newLot.description = data.description;
+        newLot.price = data.price;
+        newLot.save(function(err){
+            if(err){
+                console.log(err);
+                return;
+            }
+            res.redirect('/lots');
+        })        
+        return;
+    });
+
 
 app.get('/', function(req, res){
     res.render('main');
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function(){
